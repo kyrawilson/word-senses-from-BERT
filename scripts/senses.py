5ddcffc1961bac69457540b45895213ba5f4845f
kyrawilson <kyra.e.wilson22@gmail.com>
2022-08-18 14:13:13 +0400

    add meaning functionality

--- scripts/senses.py
+++ scripts/senses.py
@@ -11,7 +11,7 @@ import plotly.express as px
 import numpy as np
 import hdbscan
 import faulthandler
-
+import argparse
 #faulthandler.enable()
 
 #TODO: add sample parameter?
@@ -32,9 +32,9 @@ def sample_sents(corpus_path, word, n):
     return sample
 
 #TODO: add model name parameter
-def get_context_embeddings(sample):
+def get_context_embeddings(sample, model):
     embed_dict = {}
-    embedding = TransformerWordEmbeddings('bert-base-cased')
+    embedding = TransformerWordEmbeddings(model)
     for sent in sample:
         sentence = Sentence(sent)
         embedding.embed(sentence)
@@ -44,7 +44,7 @@ def get_context_embeddings(sample):
     return embed_dict
 
 #Reduce dimensions of BERT embeddings for better clustering performance
-def reduce_dim(embed_dict):
+def reduce_dim(embed_dict, tsne):
     data = []
     sents = []
     for key in embed_dict.keys():
@@ -52,34 +52,93 @@ def reduce_dim(embed_dict):
         sents.append(key)
     data = np.array(data)
     sents = np.array(sents)
-    tsne = PCA(n_components=2, random_state=0)
-    projections = tsne.fit_transform(data)
+    if tsne:
+        reduce = TSNE(n_components=2, random_state=0)
+        projections = reduce.fit_transform(data)
+    else:
+        reduce = PCA(n_components=2, random_state=0)
+        projections = reduce.fit_transform(data)
     return sents, projections
 
 #TODO: should also save all words with senses somehow
-def make_clusters(sents, projections):
-    clusterer = hdbscan.HDBSCAN(min_cluster_size=max([2, int(len(sents)*0.01)]))
+def make_clusters(sents, projections, minsize):
+    if minsize is False:
+        minsize =  int(len(sents)*0.01)
+    clusterer = hdbscan.HDBSCAN(min_cluster_size=max([2, minsize]))
     cluster_labels = clusterer.fit_predict(projections)
     projections_t = np.transpose(projections)
     data = np.stack((sents, cluster_labels, projections_t[0], projections_t[1])).T
-    df = pd.DataFrame(data, columns=['sentence', 'cluster_label', 'dim1', 'dim2'])
-    df.to_csv(f'results/cluster_points/{word}.csv', index=False)
+    df = pd.DataFrame(data, columns=['sentence', 'sense_labels', 'dim1', 'dim2'])
+    clusters = int(df['sense_labels'].max()) + 1
+    if -1 in df['sense_labels']:
+        clusters -= 1
+    df.to_csv(f'results/sense_points/{word}_{clusters}.csv', index=False)
+    return clusters
 
 #Make a plotly plot of the clusters for a given word
-def plot_senses(word):
-    df = pd.read_csv(f'results/cluster_points/{word}.csv')
-    df["cluster_label"] = df["cluster_label"].astype(str)
+def plots(word, clusters, type):
+    df = pd.read_csv(f'results/{type}_points/{word}_{clusters}.csv')
+    df[f"{type}_labels"] = df[f"{type}_labels"].astype(str)
 
     fig = px.scatter(df, x='dim1', y='dim2',
-    color='cluster_label', hover_data=['sentence'], title=word
-    )
-    fig.write_html(f"results/plots/{word}.html")
+    color=f"{type}_labels", hover_data=['sentence'], title=f"{word} {type} clusters")
+    fig.write_html(f"results/{type}_plots/{word}_{clusters}.html")
 
-corpus_path = 'data/sample_corpus.txt'
-word = sys.argv[1]
-print(f"Calculating senses for '{word}'...")
-sample = sample_sents(corpus_path, word, 1000)
-embed_dict = get_context_embeddings(sample)
-sents, projections = reduce_dim(embed_dict)
-make_clusters(sents, projections)
-plot_senses(word)
+def meanings(word, clusters, minsize):
+    df = pd.read_csv(f'results/sense_points/{word}_{clusters}.csv')
+    if minsize is False:
+        minsize = int(len(df['sentence'])*0.01)
+    projections = df[['dim1', 'dim2']]
+    clusterer = hdbscan.HDBSCAN(min_cluster_size=max([2, minsize])).fit(projections)
+    exemplars_2d = clusterer.exemplars_
+
+    if exemplars_2d:
+        min_size = min([len(sublist) for sublist in exemplars_2d])
+
+        exemplars_2d = [item for sublist in exemplars_2d for item in sublist]
+        exemplars_2d = np.array(exemplars_2d)
+        clusterer = hdbscan.HDBSCAN(min_cluster_size=min_size, allow_single_cluster=True)
+        labels = clusterer.fit_predict(exemplars_2d)
+
+        projections_t = np.transpose(exemplars_2d)
+        data = np.stack((projections_t[0], projections_t[1], labels)).T
+        df2 = pd.DataFrame(data, columns=["dim1", "dim2", "meaning_labels"])
+        x = df.merge(df2, on=["dim1", "dim2"], how="inner")
+        meaning_clusters = int(x['meaning_labels'].max()) + 1
+        if -1 in x['meaning_labels']:
+            meaning_clusters -= 1
+        x[['sentence', 'meaning_labels', 'dim1', 'dim2']].to_csv(f'results/meaning_points/{word}_{int(meaning_clusters)}.csv', index=False)
+
+    else:
+        print(f"Could not calculate number of meanings for {word}; insufficient sense clusters.")
+        clusters = 0
+
+    if clusters > meaning_clusters:
+        clusters = meaning_clusters
+
+    return int(clusters)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("word", help="the word to get the number of senses for")
+    parser.add_argument("corpus", help="path to the corpus file")
+    parser.add_argument("--sample", type=int, help="number of sentences to sample from corpus", default=1000)
+    parser.add_argument("--plots", type=bool, help="make plots of sampled sentences and clusters", default=False)
+    parser.add_argument("--tsne", type=bool, help="use tSNE for dimensionality reduction (default PCA)", default=False)
+    parser.add_argument("--meanings", type=bool, help="calculate number of meanings as well", default=False)
+    parser.add_argument("--model", help="name of contextual embedding model to use", default='bert-base-cased')
+    parser.add_argument("--minsize", type=int, help="minimum size for formation of sense clusters", default=False)
+    args = parser.parse_args()
+
+    corpus_path = args.corpus
+    word = args.word
+    sample = sample_sents(corpus_path, word, args.sample)
+    embed_dict = get_context_embeddings(sample, args.model)
+    sents, projections = reduce_dim(embed_dict, args.tsne)
+    sense_clusters = make_clusters(sents, projections, args.minsize)
+    if args.plots:
+        plots(word, sense_clusters, 'sense')
+    if args.meanings:
+        meaning_clusters = meanings(word, sense_clusters, args.minsize)
+    if args.meanings and args.plots and meaning_clusters != 0:
+        plots(word, meaning_clusters, 'meaning')
--- workflow.sh
+++ workflow.sh
@@ -8,15 +8,19 @@
 #(Optional) Make a plot of the clustering
 
 #!/bin/bash
+mkdir results/sense_points
+mkdir results/meaning_points
+mkdir results/sense_plots
+mkdir results/meaning_plots
 
-mkdir results/cluster_points
-mkdir results/plots
+parallel --bar -a data/sample_words.txt python scripts/senses.py {} data/sample_corpus.txt --meanings True --plots True
 
-parallel -a words.txt python scripts/senses.py
+# FILE="data/sample_words.txt"
+# LINES=$(cat $FILE)
+# for LINE in $LINES
+# do
+#  python scripts/senses.py $LINE data/sample_corpus.txt
+# done
 
-#FILE="words.txt"
-#LINES=$(cat $FILE)
-#for LINE in $LINES
-#do
-#  python scripts/senses.py $LINE
-#done
+ls results/sense_points | sed -e 's/_/,/g;s/.csv//g' > results/senses.csv
+ls results/meaning_points | sed -e 's/_/,/g;s/.csv//g' > results/meanings.csv
